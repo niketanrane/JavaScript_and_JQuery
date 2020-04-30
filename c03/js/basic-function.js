bdc76d6fa1fbf156c0d3bc1976d61b7e9c802c2b
Niketan Santosh Rane <40956529+niketanrane@users.noreply.github.com>
2020-05-01 01:01:55 +0530

    Add files via upload
    Added code to understand functions

---
+++ c03/js/basic-function.js
@@ -0,0 +1,10 @@
+// author: Niketan Rane 
+
+var msg = "Sign up to receive our newletter for 10% off."
+
+function updateMessage(){
+    var el = document.getElementById('message');
+    el.textContent = msg;
+}
+
+updateMessage();
